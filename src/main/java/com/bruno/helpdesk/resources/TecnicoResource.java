9e096bd8c137218fc1c548cbacfb84bb2888cd4c
Bruno Bandeira <brunobandeira20@gmail.com>
2023-08-01 07:59:34 -0300

    add methods resource

--- src/main/java/com/bruno/helpdesk/resources/TecnicoResource.java
+++ src/main/java/com/bruno/helpdesk/resources/TecnicoResource.java
@@ -1,12 +1,20 @@
 package com.bruno.helpdesk.resources;
 
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.bruno.helpdesk.domain.Tecnico;
 import com.bruno.helpdesk.domain.dtos.TecnicoDTO;
 import com.bruno.helpdesk.services.TecnicoService;
 
@@ -20,5 +28,20 @@ public class TecnicoResource {
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id){
 		return ResponseEntity.ok().body(new TecnicoDTO(tecnicoService.findById(id)));
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<TecnicoDTO>> findAll(){
+		List<Tecnico> tecnicos = tecnicoService.findAll();
+		List<TecnicoDTO> tecnicosDTO = tecnicos.stream().map(x -> new TecnicoDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(tecnicosDTO);
+	}
+	
+	@PostMapping
+	public ResponseEntity<TecnicoDTO> create(@RequestBody TecnicoDTO tecnicoDTO){
+		Tecnico tecnico = tecnicoService.create(tecnicoDTO);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(tecnico.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
--- src/main/java/com/bruno/helpdesk/services/TecnicoService.java
+++ src/main/java/com/bruno/helpdesk/services/TecnicoService.java
@@ -1,9 +1,12 @@
 package com.bruno.helpdesk.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.bruno.helpdesk.domain.Tecnico;
+import com.bruno.helpdesk.domain.dtos.TecnicoDTO;
 import com.bruno.helpdesk.repositories.TecnicoRepository;
 import com.bruno.helpdesk.services.exceptions.ObjectNotFoundExeception;
 
@@ -16,5 +19,15 @@ public class TecnicoService {
 	public Tecnico findById(Integer id) {
 		return tecnicoRepository.findById(id).
 				orElseThrow(() -> new ObjectNotFoundExeception("Objeto não encontrado: " + id));
+	}
+	
+	public List<Tecnico> findAll(){
+		return tecnicoRepository.findAll();
+	}
+
+	public Tecnico create(TecnicoDTO tecnicoDTO) {
+		tecnicoDTO.setId(null);
+		Tecnico tecnico = new Tecnico(tecnicoDTO);
+		return tecnicoRepository.save(tecnico);
 	}
 }
